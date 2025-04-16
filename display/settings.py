4892fdd81520926a59039786ff884b6c1cf74608
sarshoem <sariah.shoemaker@du.edu>
2025-04-16 13:16:49 -0600

    Debug false

--- display/settings.py
+++ display/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-667#!zrf$whbw@4apw+v7k#q5o7j!*-^3(4!$+$kmg0mzn4h$3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
